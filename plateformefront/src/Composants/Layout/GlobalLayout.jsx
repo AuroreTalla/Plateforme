6ad77be2e03201ef41836daa338ca44873fbda0c
AuroreTalla <aurorengadjou@mail.com>
2026-04-07 02:13:14 +0100

    deploit

--- plateformefront/src/Composants/Layout/GlobalLayout.jsx
+++ plateformefront/src/Composants/Layout/GlobalLayout.jsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <div className="h-screen flex flex-col">
       <NavBar />
-      <div className="flex-1 overflow-hidden">
+      <div className="flex-1 overflow-y-auto">
         <Outlet />
       </div>
     </div>
